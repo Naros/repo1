ad2644431e0f51a87b4f6040b2837e6e29ec5df2
Chris Cranford <chris@hibernate.org>
2019-09-16 20:55:44 -0400

    Externalized script call

---
+++ .github/scripts/build_website.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+curl -X POST "Accept application/vnd.github.everest-preview+json" -H "Content-Type: application/json" -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/Naros/repo2/dispatches --data '{"event_type": "documentation-update"}'
